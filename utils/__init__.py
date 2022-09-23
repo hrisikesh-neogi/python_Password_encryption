b479b42e0e63df9031059d08334ff6fb245cb3a4
hrisikesh-neogi <hrisikesh.neogi@gmail.com>
2022-09-23 16:51:28 +0530

    integrated docker

--- utils/__init__.py
+++ utils/__init__.py
@@ -18,7 +18,9 @@ def Generate_encryption_key():
         backend=backend
     )
     env_key = os.environ.get("KEY")
-    key = base64.urlsafe_b64encode(kdf.derive(b"{}".format(env_key)))
+    env_key_byte = bytes(env_key, 'utf-8')
+    
+    key = base64.urlsafe_b64encode(kdf.derive(env_key_byte))
 
     key = Fernet(key)
     
